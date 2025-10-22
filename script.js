b360b30a84d028fa96d1ebea49e10b5c42b164aa
Marcelo Manzano <manzanomarcelo@outlook.com>
2025-10-22 19:29:02 +0200

    add all the files

---
+++ script.js
@@ -0,0 +1,126 @@
+"use strict";
+const time = document.querySelector(".timer");
+const cardContainers = document.querySelectorAll(".card-container");
+const btn = document.querySelector(".btn");
+const clicks = document.querySelector(".clicks");
+let startTime = 0;
+let elapsedTime = 0;
+let mins = 0;
+let secs = 0;
+let game = false;
+let intervalId;
+let flippedCards = [];
+let cont = 0;
+let clickCounter = 0;
+function updateTime() {
+  elapsedTime = Date.now() - startTime;
+  secs = Math.floor((elapsedTime / 1000) % 60);
+  mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+
+  secs = pad(secs);
+  mins = pad(mins);
+  time.textContent = `${mins}:${secs}`;
+  function pad(unit) {
+    /* if (unit === 0) {
+      return (unit = "00");
+    }*/
+    return ("0" + unit).length > 2 ? unit : "0" + unit;
+  }
+  if (cont === 10) {
+    clearInterval(intervalId);
+    game = false;
+    elapsedTime = Date.now() - startTime;
+  }
+  console.log(time.textContent);
+}
+function handleCardClick() {
+  if (!game) {
+    game = true;
+    startTime = Date.now() - elapsedTime;
+    intervalId = setInterval(updateTime, 1000);
+  }
+  this.classList.toggle("flip");
+  flippedCards.push(this);
+  removeClickEventListeners();
+  clickCounter++;
+  clicks.textContent = `Clicks: ${clickCounter}`;
+  time.textContent = `${mins}:${secs}`;
+  if (flippedCards.length === 2) {
+    cardContainers.forEach((card) =>
+      card.removeEventListener("click", handleCardClick)
+    );
+    const img1 = flippedCards[0].querySelector(".card-front img").src;
+    const img2 = flippedCards[1].querySelector(".card-front img").src;
+    if (img1 === img2) {
+      setTimeout(() => {
+        flippedCards.forEach((card) => {
+          card.classList.add("matched");
+        });
+        flippedCards = [];
+        addClickEventListenersToCards();
+        removeClickEventListeners();
+        cont++;
+      }, 500);
+    } else {
+      setTimeout(() => {
+        flippedCards.forEach((card) => {
+          card.classList.remove("flip");
+        });
+        addClickEventListenersToCards();
+        flippedCards = [];
+      }, 1000);
+    }
+  }
+}
+function addClickEventListenersToCards() {
+  cardContainers.forEach((card) =>
+    card.addEventListener("click", handleCardClick)
+  );
+}
+function removeClickEventListeners() {
+  cardContainers.forEach((card) => {
+    if (card.classList.contains("matched") || card.classList.contains("flip")) {
+      card.removeEventListener("click", handleCardClick);
+    } else {
+      card.addEventListener("click", handleCardClick);
+    }
+  });
+}
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+const cardsContainer = document.querySelector(".cards-section");
+const cards = Array.from(document.querySelectorAll(".card"));
+const shuffledCards = shuffleArray(cards);
+shuffledCards.forEach((card) => {
+  cardsContainer.appendChild(card);
+});
+cardContainers.forEach((card) =>
+  card.addEventListener("click", handleCardClick)
+);
+btn.addEventListener("click", function () {
+  clearInterval(intervalId);
+  shuffleArray(cards);
+  startTime = 0;
+  elapsedTime = 0;
+  mins = 0;
+  secs = 0;
+  clickCounter = 0;
+  game = false;
+  flippedCards = [];
+  cont = 0;
+  time.textContent = "00:00";
+  clicks.textContent = "Clicks: 0";
+  cardContainers.forEach((card) => {
+    card.classList.remove("flip", "matched");
+  });
+  addClickEventListenersToCards();
+  const shuffledCards = shuffleArray(cards);
+  shuffledCards.forEach((card) => {
+    cardsContainer.appendChild(card);
+  });
+});
